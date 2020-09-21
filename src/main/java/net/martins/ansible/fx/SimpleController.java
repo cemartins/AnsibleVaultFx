d1e9794886a4b05f5527c7b247cca3951cf3decc
Carlos Martins <Carlos.Martins@ing.com>
2020-09-21 11:16:42 +0200

    Added alerts to display errors

--- src/main/java/net/martins/ansible/fx/SimpleController.java
+++ src/main/java/net/martins/ansible/fx/SimpleController.java
@@ -2,11 +2,9 @@ package net.martins.ansible.fx;
 
 import javafx.application.HostServices;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.nio.charset.CharacterCodingException;
@@ -15,6 +13,8 @@ import java.nio.charset.CharacterCodingException;
 public class SimpleController {
 
     private final HostServices hostServices;
+
+    private Alert alert;
 
     @FXML
     public TextArea textArea;
@@ -38,27 +38,64 @@ public class SimpleController {
     public void decryptTextArea() {
         VaultEncryptedParser vaultEncryptedParser = new VaultEncryptedParser();
         try {
-            vaultEncryptedParser.parseEncryptedText(textArea.getText());
-            textArea.setText(vaultEncryptedParser.getDecryptedVault(passwordTextField.getText()));
+            vaultEncryptedParser.parseEncryptedText(getTextAreaContent("Decrypt"));
+            textArea.setText(vaultEncryptedParser.getDecryptedVault(getPassword()));
         } catch (IOException e) {
             e.printStackTrace();
+            alert.setAlertType(Alert.AlertType.ERROR);
+            alert.setHeaderText("Decrypt");
+            alert.setContentText(e.getMessage());
+            alert.show();
         }
     }
 
     public void encryptTextArea() {
         VaultDecryptedParser vaultDecryptedParser = new VaultDecryptedParser();
         try {
-            vaultDecryptedParser.parseEncryptedText(textArea.getText());
-            textArea.setText(vaultDecryptedParser.getEncryptedVault(passwordTextField.getText()));
-        } catch (CharacterCodingException e) {
-            e.printStackTrace();
+            vaultDecryptedParser.parseEncryptedText(getTextAreaContent("Encrypt"));
+            textArea.setText(vaultDecryptedParser.getEncryptedVault(getPassword()));
         } catch (IOException e) {
             e.printStackTrace();
+            alert.setAlertType(Alert.AlertType.ERROR);
+            alert.setHeaderText("Encrypt");
+            alert.setContentText(e.getMessage());
+            alert.show();
         }
+    }
+
+    /**
+     * Checks that the password field has been filled in.
+     * @return The passwords that was filled in
+     */
+    private String getPassword() {
+        String password = passwordTextField.getText();
+        if(!StringUtils.hasText(password)) {
+            alert.setAlertType(Alert.AlertType.ERROR);
+            alert.setHeaderText("Password");
+            alert.setContentText("Please fill in the password field.");
+            alert.show();
+            passwordTextField.requestFocus();
+            throw new IllegalStateException("Password not filled in");
+        }
+        return password;
+    }
+
+    private String getTextAreaContent(String operation) {
+        String text = textArea.getText();
+        if(!StringUtils.hasText(text)) {
+            alert.setAlertType(Alert.AlertType.ERROR);
+            alert.setHeaderText(operation);
+            alert.setContentText("Please fill in the text area.");
+            alert.show();
+            textArea.requestFocus();
+            throw new IllegalStateException("Text Area not filled in");
+        }
+        return text;
     }
 
     @FXML
     public void initialize() {
+        this.alert = new Alert(Alert.AlertType.NONE);
         this.encryptButton.setOnAction(actionEvent -> this.encryptTextArea());
         this.decryptButton.setOnAction(actionEvent -> this.decryptTextArea());
     }
